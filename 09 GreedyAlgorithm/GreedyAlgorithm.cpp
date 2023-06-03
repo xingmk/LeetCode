1c10e7ae79343c27fcd3c41d0164d0961de30d6f
Edison_Xing <97339830+Xingmk@users.noreply.github.com>
2023-06-03 13:59:35 +0800

    贪心算法

--- 09 GreedyAlgorithm/GreedyAlgorithm.cpp
+++ 09 GreedyAlgorithm/GreedyAlgorithm.cpp
@@ -87,13 +87,78 @@ class Greedy {
 			}
 			return ans;
 		}
+		// 1005 .K次取反后最大化的数组和
+		static bool cmp(int a, int b) {
+			return abs(a) > abs(b);
+		}
+		int largestSumAfterKNegations(vector<int>& A, int K) {
+			sort(A.begin(), A.end(), cmp);       // 第一步
+			for (int i = 0; i < A.size(); i++) { // 第二步
+				if (A[i] < 0 && K > 0) {
+					A[i] *= -1;
+					K--;
+				}
+			}
+			if (K % 2 == 1) A[A.size() - 1] *= -1; // 第三步
+			int result = 0;
+			for (int a : A) result += a;        // 第四步
+			return result;
+		}
 		
+		// 134 . 加油站
+		// 暴力
+		int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+			for (int i = 0; i < cost.size(); i++) {
+				int rest = gas[i] - cost[i]; // 记录剩余油量
+				int index = (i + 1) % cost.size();
+				while (rest > 0 && index != i) { // 模拟以i为起点行驶一圈（如果有rest==0，那么答案就不唯一了）
+					rest += gas[index] - cost[index];
+					index = (index + 1) % cost.size();
+				}
+				// 如果以i为起点跑一圈，剩余油量>=0，返回该起始位置
+				if (rest >= 0 && index == i) return i;
+			}
+			return -1;
+		}
+		// 贪心
+		int canCompleteCircuits(vector<int>& gas, vector<int>& cost) {
+			int curSum = 0;
+			int min = INT_MAX; // 从起点出发，油箱里的油量最小值
+			for (int i = 0; i < gas.size(); i++) {
+				int rest = gas[i] - cost[i];
+				curSum += rest;
+				if (curSum < min) {
+					min = curSum;
+				}
+			}
+			if (curSum < 0) return -1;  // 情况1
+			if (min >= 0) return 0;     // 情况2
+			// 情况3
+			for (int i = gas.size() - 1; i >= 0; i--) {
+				int rest = gas[i] - cost[i];
+				min += rest;
+				if (min >= 0) {
+					return i;
+				}
+			}
+			return -1;
+		}
 		
-		
-		
-		
-		
-		
+		int canCompleteCircuitss(vector<int>& gas, vector<int>& cost) {
+			int curSum = 0;
+			int totalSum = 0;
+			int start = 0;
+			for (int i = 0; i < gas.size(); i++) {
+				curSum += gas[i] - cost[i];
+				totalSum += gas[i] - cost[i];
+				if (curSum < 0) {   // 当前累加rest[i]和 curSum一旦小于0
+					start = i + 1;  // 起始位置更新为i+1
+					curSum = 0;     // curSum从0开始
+				}
+			}
+			if (totalSum < 0) return -1; // 说明怎么走都不可能跑一圈了
+			return start;
+		}
 	};
 	
 	int main() {
